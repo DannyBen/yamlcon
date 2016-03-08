04dd8efcab00563eb68bdf1e57a3be1338337c37
DannyB <dannyb@telesofia.com>
2016-03-08 21:04:46 +0000

    spec: hello world

---
+++ spec/README.md
@@ -0,0 +1,5 @@
+# Tests
+
+To test, run `run spec` or `run spec NAME` from the repository's root folder.
+
+
---
+++ spec/yaml_config/config_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe "yaml_config" do
+
+  it "should say hello" do
+    expect(true).to be true
+  end
+
+end
