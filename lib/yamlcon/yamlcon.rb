d7fc168a98b12844b9fd49baf87f3485c1d983a5
DannyB <dannyb@telesofia.com>
2016-03-18 18:42:15 +0000

    add ability to save

--- lib/yamlcon/yamlcon.rb
+++ lib/yamlcon/yamlcon.rb
@@ -4,14 +4,32 @@ require 'ostruct'
 module YAMLCon
   def self.load_config(file)
     hash = YAML.load_file file
-    dot_notation_from hash
+    hash_to_struct hash
   end
 
-  def self.dot_notation_from(hash)
+  def self.save_config(file, data)
+    File.open file, 'w' do |f| 
+      f.write struct_to_yaml data
+    end
+  end
+
+  def self.hash_to_struct(hash)
     dotted = OpenStruct.new hash
     hash.each do |k, v| 
-      dotted[k] = dot_notation_from(v) if v.is_a? Hash
+      dotted[k] = hash_to_struct(v) if v.is_a? Hash
     end
     dotted
+  end
+
+  def self.struct_to_hash(dot_notation)
+    hash = {}
+    dot_notation.each_pair do |k, v| 
+      hash[k.to_s] = v.is_a?(OpenStruct) ? struct_to_hash(v) : v
+    end
+    hash
+  end
+
+  def self.struct_to_yaml(dot_notation)
+    struct_to_hash(dot_notation).to_yaml
   end
 end
